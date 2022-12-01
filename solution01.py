c763bc7677042e6455e288df9104beb19261b4b7
Emily Robinson <robinson.es@gmail.com>
2022-12-01 09:35:39 -0700

    update solution to strip \n

--- solution01.py
+++ solution01.py
@@ -1,19 +1,18 @@
 with open('input01.txt') as f:
-    lines = f.readlines()
+    lines = [l.rstrip("\n") for l in f]
 
 ### Problem 1
 calories = 0 
 total_calories = []
 for i in lines:
-  if i.startswith("\n"):
+  if i == '':
     total_calories.append(calories)
     calories = 0
   else:
-    calories += int(i.replace("\n", ""))
+    calories += int(i)
 
 max(total_calories)
 
 ### Problem 2
 total_calories.sort()
 sum(total_calories[-3:])
-
