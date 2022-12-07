c50ac328d5b43becfc17ba13799d7ffebc23fa63
Emily Robinson <robinson.es@gmail.com>
2022-12-07 16:02:54 -0700

    get working solution

--- solution07.py
+++ solution07.py
@@ -1,4 +1,5 @@
 import re 
+import copy 
 
 def flatten(list_of_lists):
     if len(list_of_lists) == 0:
@@ -11,18 +12,45 @@ with open('input07.txt') as f:
   command_inputs = [l.strip() for l in f]
 
 list_commands_index = [i for i, l in enumerate(command_inputs) if l == "$ ls"]
+cd_commands = [l.removeprefix("$ cd ") for l in command_inputs if l.startswith("$ cd")]
 
 dir_contents = {}
+dir_paths = [[] for i in range(len(list_commands_index))]
+
 for i, v in enumerate(list_commands_index):
-  if i < len(list_commands_index) - 1:
+  if i == 0:
     next_index = list_commands_index[i+1]
-    dir_contents[command_inputs[v-1].removeprefix("$ cd ")] = command_inputs[v+1:next_index-1]
-  else:
-    dir_contents[command_inputs[v-1].removeprefix("$ cd ")] = command_inputs[v+1:]
+    dir_contents["/"] = [c.replace("dir ", "//") for c in command_inputs[v+1:next_index-1]]
+    dir_paths[i] = ["/"]
+  elif 0 < i < len(list_commands_index) - 1:
+    next_index = list_commands_index[i+1]
+    prv_index = list_commands_index[i-1]
+    prv_cds = [s.removeprefix("$ cd ") for s in command_inputs[prv_index+1:v] if s.startswith("$ cd ")]
+    dir_paths[i] = copy.deepcopy(dir_paths[i-1])
+    for p in prv_cds: 
+      if p != "..":
+        dir_paths[i].append(p)
+      elif p == "..":
+        dir_paths[i] = dir_paths[i][:-1]
+    dir_name = '/'.join(dir_paths[i])
+    dir_ent = [c.replace("dir ", dir_name + "/") for c in command_inputs[v+1:next_index-1]]
+    dir_contents[dir_name] = dir_ent
+  elif i == len(list_commands_index) - 1:
+    prv_index = list_commands_index[i-1]
+    prv_cds = [s.removeprefix("$ cd ") for s in command_inputs[prv_index+1:v] if s.startswith("$ cd ")]
+    dir_paths[i] = copy.deepcopy(dir_paths[i-1])
+    for p in prv_cds: 
+      if p != "..":
+        dir_paths[i].append(p)
+      elif p == "..":
+        dir_paths[i] = dir_paths[i][:-1]
+    dir_name = '/'.join(dir_paths[i])
+    dir_ent = [c.replace("dir ", dir_name + "/") for c in command_inputs[v+1:next_index-1]]
+    dir_contents[dir_name] = dir_ent
 
 dict_within_dict = {}
 for k, e in dir_contents.items():
-  dict_within_dict[k] = [v.removeprefix("dir ") for v in e if v.startswith("dir")] + [int(re.findall(r'\d+', v)[0]) for v in e if re.findall(r'\d+', v) != []]
+  dict_within_dict[k] = [v for v in e if v.startswith("//")] + [int(re.findall(r'\d+', v)[0]) for v in e if re.findall(r'\d+', v) != []]
 
 while not all(isinstance(value, int) for value in dict_within_dict.values()):
   for k, e in dict_within_dict.items(): 
@@ -33,9 +61,7 @@ while not all(isinstance(value, int) for value in dict_within_dict.values()):
 
 sum([v for v in dict_within_dict.values() if v <= 100000])
 
-# debug
-## FFFF DIRECTORIES HAVE THE SAME NAME
+# unused space
+space_to_delete = 30000000 - (70000000 - dict_within_dict["/"])
 
-cd_commands = [(i, l.removeprefix("$ cd ")) for i, l in enumerate(command_inputs) if l.startswith("$ cd") and l != "$ cd .."]
-
-command_dir = [x[1] for x in cd_commands]
+min([v for v in dict_within_dict.values() if v > space_to_delete])
