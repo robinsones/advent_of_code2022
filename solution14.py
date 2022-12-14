5766a8fbf981ed06c43b1e4de734c45b58422adf
Emily Robinson <robinson.es@gmail.com>
2022-12-14 11:23:30 -0700

    simplify day 14

--- solution14.py
+++ solution14.py
@@ -1,4 +1,6 @@
 from collections import defaultdict
+import copy
+
 lines = []
 with open("input14.txt") as f:
   for ind, l in enumerate(f):
@@ -8,20 +10,7 @@ with open("input14.txt") as f:
       cur_ent.append(tuple(map(int, i.split(','))))
     lines.append(cur_ent)
 
-# how deep does the cave go? It's the max of y
-
 max_y = max([y for pt in lines for _, y in pt])
-max_x = max([x for pt in lines for x, _ in pt])
-min_x = min([x for pt in lines for x, _ in pt])
-
-# sand starts from point 500, 0 
-# it wants to go down until it hits a rock, then tries to go down left, then down right
-# Possible approaches: 
-# - make a matrix to represent the graph
-
-# maybe while the next one is unoccupied, go down one? 
-# if can't go down left, if can't go down right, if can't do any stop?
-# dictionary of tuples with "rock" "empty" or "sand"?
 
 def draw_line(left, right, graph_d):
   sm_horiz = min(left[0], right[0])
@@ -39,8 +28,9 @@ for path_set in lines:
     if len_p <= len(path_set)-2:
       draw_line(path_set[len_p], path_set[len_p+1], og_graph)
 
-# it can't fall off map 
-def sand_falling(gr, max_depth):
+copy_og_graph = copy.deepcopy(og_graph)
+
+def sand_falling(gr, max_depth, cur_pos):
   if cur_pos[1] == max_depth:
     return True, cur_pos
   elif gr[(cur_pos[0], cur_pos[1]+1)] == '':
@@ -63,7 +53,7 @@ def one_grain(og_graph, max_depth):
     if bool_val:
       break
 
-for _ in range(3000):
+for _ in range(2000):
   one_grain(og_graph, max_y)
   
 len([k for k, v in og_graph.items() if v == "sand"])
@@ -71,11 +61,6 @@ len([k for k, v in og_graph.items() if v == "sand"])
 ## Part 2
 
 floor = max_y + 2
-p2_graph = defaultdict(str) 
-for path_set in lines:
-  for len_p in range(len(path_set)):
-    if len_p <= len(path_set)-2:
-      draw_line(path_set[len_p], path_set[len_p+1], p2_graph)
 
 def sand_fallingp2(gr, max_depth, cur_pos):
   if cur_pos[1] == max_depth-1:
@@ -101,7 +86,7 @@ def one_grainp2(og_graph, max_depth):
     if bool_val:
       break
 
-while p2_graph[(500, 0)] != "sand":
-  one_grainp2(p2_graph, floor)
+while copy_og_graph[(500, 0)] != "sand":
+  one_grainp2(copy_og_graph, floor)
   
-len([k for k, v in p2_graph.items() if v == "sand"])
+len([k for k, v in copy_og_graph.items() if v == "sand"])
