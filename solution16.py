31e4602bffe5f5d76da695e1b73637f85e474afb
Emily Robinson <robinson.es@gmail.com>
2022-12-16 16:18:55 -0700

    simplify code

--- solution16.py
+++ solution16.py
@@ -14,7 +14,7 @@ with open('ex_input16.txt') as f:
       pt = [i.strip() for i in l.strip().split("leads to valve ",1)[1].split(',')]
     valve_and_paths[cur_valve] = {'flow_rate': fr[0], 'paths_to': pt}
 
-def find_path_to_endm(p_dict, cur_node, time_remaining, running_sum, cur_m, valves_running):
+def find_path_to_end(p_dict, cur_node, time_remaining, running_sum, cur_m, valves_running):
   if time_remaining <= 1:
     return max(running_sum, cur_m)
   next_nodes = p_dict[cur_node]['paths_to']
@@ -23,18 +23,18 @@ def find_path_to_endm(p_dict, cur_node, time_remaining, running_sum, cur_m, valv
   if frv == 0 or cur_node in valves_running:
     for valve_to_open in next_nodes:
       time_remaining -= 1
-      return find_path_to_endm(p_dict, valve_to_open, time_remaining, running_sum, cur_m, valves_running)
+      return find_path_to_end(p_dict, valve_to_open, time_remaining, running_sum, cur_m, valves_running)
   else:
     time_remaining -= 1
     running_sum += frv * (time_remaining)
     valves_running.append(cur_node)
     passed_vr = copy.deepcopy(valves_running)
-    return find_path_to_endm(p_dict, cur_node, time_remaining, running_sum, cur_m, passed_vr)
+    return find_path_to_end(p_dict, cur_node, time_remaining, running_sum, cur_m, passed_vr)
     for valve_to_open in next_nodes:
-      return find_path_to_endm(p_dict, valve_to_open, time_remaining, running_sum, cur_m, valves_running)
+      return find_path_to_end(p_dict, valve_to_open, time_remaining, running_sum, cur_m, valves_running)
 
 # this is right
-find_path_to_endm(p_dict = valve_and_paths, cur_node = 'DD', time_remaining = 3, running_sum = 0, cur_m = 0, valves_running = [])
+find_path_to_end(p_dict = valve_and_paths, cur_node = 'DD', time_remaining = 3, running_sum = 0, cur_m = 0, valves_running = [])
 
 # this should be 93 but is 84
-find_path_to_endm(p_dict = valve_and_paths, cur_node = 'AA', time_remaining = 6, running_sum = 0, cur_m = 0, valves_running = [])
+find_path_to_end(p_dict = valve_and_paths, cur_node = 'AA', time_remaining = 6, running_sum = 0, cur_m = 0, valves_running = [])
